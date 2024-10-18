7ebe79d9b7ffe9432848165a9ce43f10a43a371e
Omar Bekauri <omarbekauri13@gmail.com>
2024-10-17 22:04:57 -0400

    WIP

--- frontend/components/profile.js
+++ frontend/components/profile.js
@@ -6,6 +6,8 @@ export default function Profile() {
             <h1>Hello</h1>
             <p>This is my project</p>
             <h2> Here you can see my presonal Information</h2>
+
+            <h3>You can see my contents here</h3>
         </>
         
     );
