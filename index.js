a0addbb4193f6a28900145da73115306587f3f3f
Omar Bekauri <omarbekauri13@gmail.com>
2024-10-24 18:34:44 -0400

    WIP

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const server = require('./api/server.js');
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-    console.log(`\n*** Server Runnig on http://localhost/${port} ***n/`)
+    console.log(`Server Runnig on http://localhost/${port} ***n/`)
 });
