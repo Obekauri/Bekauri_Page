b6a046c7a5883a60560a44a21c904dcfbdc616c6
Omar Bekauri <omarbekauri13@gmail.com>
2024-11-01 02:13:02 -0400

    WIP

--- frontend/components/profile.js
+++ frontend/components/profile.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 export default function Profile() {
     return (
-        <>
+        <>  
+        <div></div>
             <h1>Hello</h1>
             <p>This is my project</p>
             <h2> Here you can see my presonal Information</h2>
@@ -11,6 +12,7 @@ export default function Profile() {
 
             <p>Here I'm going to insert my BIO</p>
 
+            
             <h3>You can see my contents here</h3>
             <botton><a href='Need link here'>You can see info here</a></botton>
         </>
