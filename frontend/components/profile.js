6d00575e4419ea0951d4552f576418e5f3a390e9
Omar Bekauri <omarbekauri13@gmail.com>
2024-11-22 23:51:51 -0500

    WIP

--- frontend/components/profile.js
+++ frontend/components/profile.js
@@ -17,8 +17,8 @@ export default function Profile() {
             </div>
 
             <div>
-            <h3>You can see my contents here</h3>
-            <botton><a href='Need link here'>You can see info here</a></botton>
+                <h3>You can see my contents here</h3>
+                <botton><a href='Need link here'>You can see info here</a></botton>
             </div>
             
 
