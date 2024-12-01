2bc3c24ba1f8ae16f807716da4537acb985a2be6
Omar Bekauri <omarbekauri13@gmail.com>
2024-12-01 14:17:38 -0500

    WIP

--- index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-const server = require('./api/server.js');
-
-const port = process.env.PORT || 3000;
-
-server.listen(port, () => {
-    console.log(`Server Runnig on http://localhost/${port}`)
-});
