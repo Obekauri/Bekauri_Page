13258a9bfd1f70719e480fee56f08d6a7372eec6
Omar Bekauri <omarbekauri13@gmail.com>
2024-12-26 21:41:45 -0500

    WIP

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ const server = require('./server.js')
 const port = precess.env.PORT || 9000;
 
 server.listen(port, () => {
-    
+    console.log(`\n*** Server Running on http://localhost/${port} ***n/`)
 })
