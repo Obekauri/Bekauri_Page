ddec86922613b7d7e2fe3467ee79d50bbb4e571a
Omar Bekauri <omarbekauri13@gmail.com>
2025-01-21 23:33:00 -0500

    WIP

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+// We'll need React Router's hook to navigate around
+import { useNavigate } from 'react-router-dom'
+
+export default function Home() {
+  const navigate = useNavigate()
+
+  // 👉 STEP 5 - Build a click handler that will imperatively navigate us to /items-list
+  const routeToShop = () => {
+    console.log('Submitting...')
+    navigate('items-list')
+  }
+
+  return (
+    <div className='home-wrapper'>
+      <img
+        className='home-image'
+        src='https://source.unsplash.com/F6-U5fGAOik'
+        alt='store front'
+      />
+      <button onClick={routeToShop} className='md-button shop-button'>
+        Shop now!
+      </button>
+    </div>
+  )
+}
