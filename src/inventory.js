ba4b04c3825d24de22777c0378f0e3c7db033e16
Omar Bekauri <omarbekauri13@gmail.com>
2025-04-08 18:47:56 -0500

    WIP

--- src/inventory.js
+++ src/inventory.js
@@ -41,7 +41,7 @@ function Inventory{
             }else if(projectManagment > 200){
                 console.log("increased more then 200")
             }else if(projectManagment > 300){
-                
+                console.log()
             }
         }
     }
