5ab35b1f178262a52d94b54cbd0be8876256d82c
Omar Bekauri <omarbekauri13@gmail.com>
2025-06-03 13:36:55 -0500

    WIP

--- src/components/Home.js
+++ src/components/Home.js
@@ -38,7 +38,7 @@ function Home() {
 
         <img src={firstImg} alt="Project preview" />
   
-        {/* Project Filter Section */}
+        {/* Project  Section */}
         <section>
           <h2 className="text-2xl font-bold mb-4">Projects</h2>
   
