50f0ce5e8996e740eec0b6cb6405d0469fa67c2f
Omar Bekauri <omarbekauri13@gmail.com>
2025-07-29 16:36:27 -0500

    WIP

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'
 
 // Importing the top-level component
 import App from './components/App'
