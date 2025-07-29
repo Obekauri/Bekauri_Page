4926c42af36e19b77289e8e1d34a7b3082b4426f
Omar Bekauri <omarbekauri13@gmail.com>
2025-07-29 16:38:13 -0500

    WIP

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 // Importing the top-level component
 import App from './components/App'
 
-// mporting the styles
+// Importing the styles
 import './styles.css'
 
 // 👉 STEP 1 - Import Router and wrap the app
