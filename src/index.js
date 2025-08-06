eb6b261812d32a540e724a112231eccbe86e5c19
Omar Bekauri <omarbekauri13@gmail.com>
2025-08-06 12:08:00 -0500

    WIP

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import './styles.css'
 // 👉 STEP 1 - Import Router and wrap the app
 import { BrowserRouter } from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root');
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <BrowserRouter>
